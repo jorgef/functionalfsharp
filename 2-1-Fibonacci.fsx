de4f1df8cbb0300b3e3b557ec44856bc1b77c2ad
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-22 09:03:56 -0500

    2.1 fibonacci

---
+++ 2-1-Fibonacci.fsx
@@ -0,0 +1,11 @@
+﻿let rec fibonacci = function
+    | 0 -> 0
+    | 1 -> 1
+    | n -> fibonacci (n-1) + fibonacci (n-2)
+
+let fibonacci' n = // tail recursive
+    let rec loop i p c =
+        match i with
+        | 0 -> p
+        | i -> loop (i-1) c (p+c) 
+    loop n 0 1
---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FunctionalFSharp.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FunctionalFSharp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FunctionalFSharp")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("648b104f-c2d2-413e-bd5c-f1050f61372a")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
