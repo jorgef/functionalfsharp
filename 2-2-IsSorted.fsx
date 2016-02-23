1e406ddc0050d0433f42b0d6e8d5fd3978fa1750
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-22 19:47:52 -0500

    2.2 IsSorted

---
+++ 2-2-IsSorted.fsx
@@ -0,0 +1,5 @@
+﻿let rec isSorted<'a> (ordered: ('a * 'a) -> bool) (array: 'a[]) : bool =
+    match (array |> Array.tryHead, array |> Array.tryItem 1) with
+    | _, None | None, _ -> true
+    | Some f, Some s when ordered (f,s) -> array |> Array.tail |> isSorted ordered
+    | _ -> false
