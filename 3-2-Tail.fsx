4c9ba3e911b5474cb1cbcd31fec6f4dc6e92344b
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-24 09:02:09 -0500

    3.2 Tail

---
+++ 3-2-Tail.fsx
@@ -0,0 +1,10 @@
+﻿type List<'a> =
+    | Nil
+    | Cons of 'a * List<'a> 
+
+let tail<'a> (list: List<'a>) : List<'a> =
+    match list with
+    | Nil -> Nil
+    | Cons(_, tail) -> tail
+
+let t = Cons(1, Cons(2, Cons (3, Nil))) |> tail
