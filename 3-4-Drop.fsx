f9fe99ab94d4fc3050eb33160176d71dc98ac5ec
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-25 08:12:10 -0500

    3.4 Drop

---
+++ 3-4-Drop.fsx
@@ -0,0 +1,11 @@
+﻿type List<'a> =
+    | Nil
+    | Cons of 'a * List<'a> 
+
+let rec drop<'a> (n: int) (list: List<'a>) : List<'a> =
+    match n, list with
+    | _, Nil -> Nil
+    | 0, l -> l
+    | n, Cons(_, t) -> drop (n - 1) t
+
+let t = Cons(1, Cons(2, Cons (3, Nil))) |> drop 2
