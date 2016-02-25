f212c8a39b2c13d5b13ecf13523768061f825ff5
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-24 19:18:01 -0500

    3.3 SetHead

---
+++ 3-3-SetHead.fsx
@@ -0,0 +1,8 @@
+﻿type List<'a> =
+    | Nil
+    | Cons of 'a * List<'a> 
+
+let setHead<'a> (head: 'a) (list: List<'a>) : List<'a> =
+    Cons(head, list)
+
+let t = Cons(1, Cons(2, Cons (3, Nil))) |> setHead 0
