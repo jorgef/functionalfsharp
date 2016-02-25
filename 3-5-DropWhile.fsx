763472a074a4066d34967e46eb2dbf58830492af
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-25 08:20:39 -0500

    3.5 DropWhile

---
+++ 3-5-DropWhile.fsx
@@ -0,0 +1,11 @@
+﻿type List<'a> =
+    | Nil
+    | Cons of 'a * List<'a> 
+
+let rec dropWhile<'a> (f: 'a -> bool) (l: List<'a>) : List<'a> =
+    match l with
+    | Nil -> Nil
+    | Cons(a, _) when not (f a) -> l
+    | Cons(_, t) -> dropWhile f t
+
+let t = Cons(1, Cons(2, Cons (3, Nil))) |> dropWhile (fun a -> a < 2)
