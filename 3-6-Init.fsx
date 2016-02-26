93c9fbf22fbd02f035942473bea55fb44f0859db
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2016-02-25 20:15:21 -0500

    3.6 Init

---
+++ 3-6-Init.fsx
@@ -0,0 +1,19 @@
+﻿type List<'a> =
+    | Nil
+    | Cons of 'a * List<'a> 
+
+let init<'a> (list: List<'a> ) : List<'a> =
+    let rec reverse res list =
+        match list with
+        | Nil -> res
+        | Cons(h, t) -> reverse (Cons(h, res)) t
+        
+    let rec loop res list =
+        match list with
+        | Nil -> Nil
+        | Cons(_, Nil) -> reverse Nil res
+        | Cons(h, t) -> loop (Cons(h, res)) t
+
+    loop Nil list
+
+let t = Cons(1, Cons(2, Cons (3, Nil))) |> init
